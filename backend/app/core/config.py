660a59076b7c1baf69cb224955737a7535e067eb
denisbyte <denisafoh22@gmail.com>
2026-09-09 17:52:44 +0200

    Ajout de la configuration

---
+++ backend/app/core/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    """ Paramètres de configuration et du pipeline RAG"""
+
+    app_name: str = "RAG Admin Assistant"
+    app_version: str = "1.0.0"
+
+    documents_path: str = "data/docuements"
+    chroma_path: str = "data/chroma"
+
+    embedding_model: str = "sentence-transformers/all-MiniLM-L6-v2"
+    ollama_base_url: str = "http://localhost:11434"
+    ollama_model: str = "deepseek-r1:7b"
+    redis_url: str = "redis://localhost:6379"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="uft-8",
+        extra="ignore"
+
+    )
+settings = Settings()
+
