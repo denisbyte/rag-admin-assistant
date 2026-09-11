6618710ea829c1e347ab8579f8882a60509b2f36
denisbyte <denisafoh22@gmail.com>
2026-09-12 00:10:07 +0200

    Modification du fichier main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from backend.app.api.routes.chat import router as chat_router
 from backend.app.api.routes.documents import router  as document_router
 
@@ -8,6 +8,20 @@ app = FastAPI(
     description="Assistant administratif basé sur RAG",
     version="1.0.0"
 )
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/health")
 def health_check():
